eae3fdf49cb73eae86ea8558f8de47cdb16ad41b
NeilVicente <neil@neilacero.com>
2018-05-11 22:06:42 +0800

    outoput cors header

--- server.php
+++ server.php
@@ -1,5 +1,9 @@
 <?php
 
+if (preg_match("/^chrome-extension:\/\/.+/", $_SERVER['HTTP_ORIGIN'])) {
+    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+}
+
 $url = $_REQUEST['url'];
 echo checkStatus($url);
 
