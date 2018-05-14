fb779df198c8c167abff19f1b04b633dc4482473
NeilVicente <neil@neilacero.com>
2018-05-15 00:38:16 +0800

    Catch errors only on main window frame

--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 'use strict';
 
 chrome.webNavigation.onErrorOccurred.addListener(function (e) {
+  if (e.frameId !== 0) { return; }
   chrome.pageAction.show(e.tabId);
   chrome.storage.local.set({ url: e.url }, function() {
     chrome.extension.getBackgroundPage().console.log('URL: ' + e.url);
