3e29f8dc81d895a790da9ae3ad5144562936da0c
NeilVicente <neil@neilacero.com>
2018-05-15 19:14:46 +0800

    Catch only 'ERR_NAME_NOT_RESOLVED' errors

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 'use strict';
 
 chrome.webNavigation.onErrorOccurred.addListener(function (e) {
-  if (e.frameId !== 0) { return; }
+  if (e.frameId !== 0 || e.error !== 'net::ERR_NAME_NOT_RESOLVED') { return; }
   chrome.pageAction.show(e.tabId);
   chrome.storage.local.set({ url: e.url }, function() {
     chrome.extension.getBackgroundPage().console.log('URL: ' + e.url);
